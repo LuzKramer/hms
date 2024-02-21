40e0d4e0d9a9ac3048f1a36d2c38debea124f4ff
William Kramer <LuzKramer>
2024-02-20 20:37:00 -0400

    hms 0.1.9 login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\site\RhController;
 use App\Http\Controllers\site\RoomsController;
@@ -22,6 +23,21 @@ use App\Http\Controllers\site\ProductController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
 
 Route::namespace(value: 'site')->group(function () {
 
